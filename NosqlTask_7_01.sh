6b40032d5e71bcb08d548abbd9aab6a809f04b5b
Chandana222 <Chandana.K.Varma@gmail.com>
2017-10-29 13:54:19 +0530

    Updated .sh file in an commands in an order

--- NosqlTask_7_01.sh
+++ NosqlTask_7_01.sh
@@ -7,7 +7,7 @@ hadoop fs -put /home/orienit/nosql/*.* /hbase/project
 
 # Importing data from HBASE to HDFS 
 
-create 'student1_csv', 'cf1'
+create 'student1_tsv', 'cf1'
 
 hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.columns=HBASE_ROW_KEY,cf1:name,cf1:course,cf1:year student1_tsv 
 hdfs://quickstart.cloudera:8020/hbase/project/student1.tsv
@@ -16,13 +16,13 @@ scan 'student1_tsv'
 
 scan 'student1_tsv', {FILTER => "RowFilter(>, 'binary:2') AND ValueFilter(=,'binary:spark')"}
 
-create 'student_tsv', 'cf2'
+create 'student1_csv', 'cf2'
 
 hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=, -Dimporttsv.columns=HBASE_ROW_KEY,cf2:name,cf2:course,cf2:year student1_csv hdfs://quickstart.cloudera:8020/hbase/project/student1.tsv
 
 scan 'student1_csv'
 
-scan 'Student2_csv', {FILTER => "RowFilter(>, 'binary:2') AND ValueFilter(=,'binary:spark')"}
+scan 'student1_csv', {FILTER => "RowFilter(>, 'binary:2') AND ValueFilter(=,'binary:spark')"}
 
 
 
